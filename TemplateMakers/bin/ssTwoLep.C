ee23c060743b4b67fd8763c3169775aca9e90f3c
Jason Slaunwhite <slaunwhj@cern.ch>
2013-09-04 08:29:06 -0400

    Added Higgs Decay from Anna, fixed lepton core to properly initialize MC variables

--- TemplateMakers/bin/ssTwoLep.C
+++ TemplateMakers/bin/ssTwoLep.C
@@ -205,6 +205,10 @@ int main (int argc, char** argv) {
    DBCorrectedRelIsoDR04s myDBCorrectedRelIsoDR04s(&lepHelper, 4);
    kinVars.push_back(&myDBCorrectedRelIsoDR04s);
 
+   HiggsDecayType hdecay(&lepHelper);
+   kinVars.push_back(&hdecay);
+   
+
 //    LepTrackCharges myLepTrackCharges(2);
 //    kinVars.push_back(&myLepTrackCharges);
 //    myLepTrackCharges.setCut("SS");
